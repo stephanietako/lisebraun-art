c8ecd6b8525110b380e8f84b942e8f97df30077a
stephanietako <gabeshine@live.fr>
2023-11-10 17:11:03 +0100

    wip: rework & refacto

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 // Assets
 import image from "../../assets/images/boueeHoriz.webp";
+import gif from "../../assets/gif/aqua.gif";
 // Styles
 import styles from "./styles.module.scss";
 import AnimCircle from "../../components/AnimCircle";
@@ -17,7 +18,24 @@ const Home = () => {
             <img src={image} alt="woman on buoy in the swimming pool" />
           </div>
         </div>
-        <div className={styles.__section}></div>
+        <div className={styles.__section}>
+          <div className={styles.__event}></div>
+          <div className={styles.__box}>
+            <div className={styles.__box_content}>
+              <div className={styles.__gif}>
+                <img src={gif} alt="event aqua art miami " />
+              </div>
+              <p>
+                "I want to continue promoting arts and special pieces
+                internationaly, meet a growing demand for hight quality and rare
+                products, build a collection based on encounters, projects
+                stories of human beings."
+              </p>
+              <div className={styles.__form_contact}>FORMULAIRE</div>
+            </div>
+          </div>
+        </div>
+
         <Footer />
       </div>
     </>
