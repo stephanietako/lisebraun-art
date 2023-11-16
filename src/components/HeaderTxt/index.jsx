18a7576cabfbefd3a96c930d5c8e4e1c827b6ae6
stephanietako <gabeshine@live.fr>
2023-11-16 01:33:42 +0100

    add textes content on component

--- src/components/HeaderTxt/index.jsx
+++ src/components/HeaderTxt/index.jsx
@@ -3,26 +3,28 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 const HeaderTxt = ({ txts }) => {
-  const txtart =
-    "I opened my first gallery at the age of 20, while studying interior design. Since then, I've been promoting emerging and established artists, both in and out of the gallery. We are present at various international contemporary art fairs. My unique guiding principle: the passion and close relationship I have with my artists.";
-  const txtdesign =
-    "In France, I have represented prestigious design editors such as Vitra, Cappellini, Agape, Roda and Bitossi. This encouraged me to produce art objects in ultra-limited series.";
-  const txtcreation =
-    "I love artistic research and inventing concepts and bridges between the arts. I regularly organize exhibitions for companies and institutions. We've linked contemporary art, perfume, haiku, Formula 1 objects and, of course, digital art. I'm an early member of Blockchain Innov.";
+  const contents = {
+    art: [
+      "I opened my first gallery at the age of 20, while studying interior design. Since then, I've been promoting emerging and established artists, both in and out of the gallery. We are present at various international contemporary art fairs. My unique guiding principle: the passion and close relationship I have with my artists.",
+    ],
+    design: [
+      "In France, I have represented prestigious design editors such as Vitra, Cappellini, Agape, Roda and Bitossi. This encouraged me to produce art objects in ultra-limited series.",
+    ],
+    creation: [
+      "I love artistic research and inventing concepts and bridges between the arts. I regularly organize exhibitions for companies and institutions. We've linked contemporary art, perfume, haiku, Formula 1 objects and, of course, digital art. I'm an early member of Blockchain Innov.",
+    ],
+  };
+
+  const content = contents[txts] || [];
 
   return (
     <div className={styles.txts}>
-      {txts}
       <ul>
-        <li>
-          <p>{txtart}</p>
-        </li>
-        <li>
-          <p> {txtdesign}</p>
-        </li>
-        <li>
-          <p> {txtcreation}</p>
-        </li>
+        {content.map((paragraph, index) => (
+          <li key={index}>
+            <p>{paragraph}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
