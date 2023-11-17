058cad379a25935e3431ead9ce545eaf04e16670
stephanietako <gabeshine@live.fr>
2023-11-17 20:59:52 +0100

    wip:rework & refacto

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "../../components/Modal";
 import menu from "../../assets/icon/menu.png";
 import cross from "../../assets/icon/cross.png";
@@ -7,55 +7,78 @@ import styles from "./styles.module.scss";
 const Header = () => {
   const [openModal, setOpenModal] = useState(false);
   const [selectedTxt, setSelectedTxt] = useState("");
-
   const [isOpen, setIsOpen] = useState(false);
-  const toggleMenu = () => setIsOpen(!isOpen);
-
   const instaURL = process.env.REACT_APP_INSTA_URL;
   const listItems = ["art", "design", "creation"];
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1200);
 
-  const ListItem = ({ text }) => (
-    <button
-      className={styles.__txt_modal}
-      onClick={() => {
-        setSelectedTxt(text);
-        setOpenModal(true);
-      }}
-    >
-      <li>{text}</li>
-    </button>
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1200);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Fonction du click ouverture du modal et du texte cortrespondant
+  const handleItemClick = (item) => {
+    setSelectedTxt(item);
+    setOpenModal(true);
+  };
+
+  // liste des textes dans les modals dans le menu principale et mobile avec le bouton pour l'ouverture du modal
+  const ListItem = ({ listItems }) => (
+    <div className={styles.__list_header}>
+      {listItems.map((item) => (
+        <button
+          key={item}
+          className={
+            isMobile ? styles.__listItem_mobile_btn : styles.__listItem_btn
+          }
+          onClick={() => handleItemClick(item)}
+        >
+          <li>{item}</li>
+        </button>
+      ))}
+    </div>
   );
+
+  // Fonction pour basculer l'état du menu mobile entre ouvert et fermé
+  const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
     <div className={styles.header}>
+      {/* Appel de l'import Modal*/}
       <Modal
         open={openModal}
         onClose={() => setOpenModal(false)}
         contentTxt={selectedTxt}
       />
+
+      {/* Desktop */}
       <div className={styles.__container_desk}>
-        <ul className={styles.__list_header}>
-          {listItems.map((item) => (
-            <ListItem key={item} text={item} />
-          ))}
+        <ul className={styles.__menu_desk}>
+          <ListItem listItems={listItems} />
         </ul>
+        {/* Lien Instagram */}
         <div className={styles.__insta_link}>
           <a href={instaURL} target="_blank" rel="noopener noreferrer">
             <p>instagram</p>
           </a>
         </div>
       </div>
+
       {/* Mobile */}
       <div className={styles.__container_mobile}>
         <button className={styles.__burger_btn} onClick={toggleMenu}>
           <img src={isOpen ? cross : menu} alt={isOpen ? "Menu" : "Cross"} />
         </button>
         {isOpen && (
-          <div className={styles.__menu_mobile}>
-            {listItems.map((item) => (
-              <ListItem key={item} text={item} />
-            ))}
-          </div>
+          <ul className={styles.__menu_mobile}>
+            <ListItem listItems={listItems} />
+          </ul>
         )}
       </div>
     </div>
