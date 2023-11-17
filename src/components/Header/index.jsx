c4305bdb8bcc63355e45a33b8642ce8132a7ecfb
stephanietako <gabeshine@live.fr>
2023-11-17 11:47:05 +0100

    wip:rework & refacto

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,32 +1,39 @@
 import React, { useState } from "react";
 import Modal from "../../components/Modal";
-// Styles
+import menu from "../../assets/icon/menu.png";
+import cross from "../../assets/icon/cross.png";
 import styles from "./styles.module.scss";
 
 const Header = () => {
   const [openModal, setOpenModal] = useState(false);
   const [selectedTxt, setSelectedTxt] = useState("");
 
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   const instaURL = process.env.REACT_APP_INSTA_URL;
   const listItems = ["art", "design", "creation"];
 
-  const ListItem = ({ text }) => {
-    return (
-      <button
-        className={styles.__txt_modal}
-        onClick={() => {
-          setSelectedTxt(text);
-          setOpenModal(true);
-        }}
-      >
-        <li>{text}</li>
-      </button>
-    );
-  };
+  const ListItem = ({ text }) => (
+    <button
+      className={styles.__txt_modal}
+      onClick={() => {
+        setSelectedTxt(text);
+        setOpenModal(true);
+      }}
+    >
+      <li>{text}</li>
+    </button>
+  );
 
   return (
     <div className={styles.header}>
-      <div className={styles.__container}>
+      <Modal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        contentTxt={selectedTxt}
+      />
+      <div className={styles.__container_desk}>
         <ul className={styles.__list_header}>
           {listItems.map((item) => (
             <ListItem key={item} text={item} />
@@ -37,11 +44,19 @@ const Header = () => {
             <p>instagram</p>
           </a>
         </div>
-        <Modal
-          open={openModal}
-          onClose={() => setOpenModal(false)}
-          contentTxt={selectedTxt}
-        />
+      </div>
+      {/* Mobile */}
+      <div className={styles.__container_mobile}>
+        <button className={styles.__burger_btn} onClick={toggleMenu}>
+          <img src={isOpen ? cross : menu} alt={isOpen ? "Menu" : "Cross"} />
+        </button>
+        {isOpen && (
+          <div className={styles.__menu_mobile}>
+            {listItems.map((item) => (
+              <ListItem key={item} text={item} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
