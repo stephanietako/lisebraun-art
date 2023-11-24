26c0ba82764d4eb9efebb8fc685183a4351f9f2c
stephanietako <gabeshine@live.fr>
2023-11-24 20:03:46 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Maintenance from "./pages/Maintenance";
+import MentionsLegales from "./pages/MentionsLegales";
+import Page404 from "./pages/Page404";
 // Styles
 import "./App.scss";
 
@@ -30,6 +32,8 @@ const App = () => {
               )
             }
           />
+          <Route path="/terms" element={<MentionsLegales />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
       </Router>
     </div>
